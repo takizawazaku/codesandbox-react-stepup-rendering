de6110ce4743c3c063ebd552cbdcf03ee23fa006
takizawazaku <96064983+takizawazaku@users.noreply.github.com>
2022-02-05 20:38:47 +0900

    Initial commit

---
+++ src/ChildArea.jsx
@@ -0,0 +1,25 @@
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = (props) => {
+  const { open } = props;
+  console.log("ChildAreaがレンダリングされた!!");
+
+  const data = [...Array(2000).keys()];
+  for (const row of data) {
+    console.log("...");
+  }
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+};
